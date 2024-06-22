2c46c525b57200fef969f1889276c742dfa717e5
ivanafilep <125295042+ivanafilep@users.noreply.github.com>
2024-06-22 16:16:21 +0200

    Add healthcheck

--- Zoo/Zoo/obj/Release/net8.0/Zoo.AssemblyInfo.cs
+++ Zoo/Zoo/obj/Release/net8.0/Zoo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zoo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+868fe04af0cafbc63d18bdd661227aa79c313050")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zoo")]
